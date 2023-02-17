e16eb102edeccc3bcb7f6ddba79b727c07a942f6
Shuyu114514 <108236326+Shuyu114514@users.noreply.github.com>
2023-02-18 00:08:01 +0800

    Add files via upload

---
+++ common/defines/cjc_adjust_defines.lua
@@ -0,0 +1,7 @@
+NDefines.NOperatives.CONTROL_TRADE_MAX_INFLUENCE = 200.0;					-- The maximum amount of trade influence that can be gained through the control trade mission
+NDefines.NCountry.MAX_PROPAGANDA_STABILITY_IMPACT = -0.4;			-- Max total penalty from operative performing the propaganda mission in a country
+NDefines.NCountry.MAX_PROPAGANDA_WAR_SUPPORT_IMPACT = -0.4;		-- Max total penalty from operative performing the propaganda mission in a country
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000;			--Max army experience a country can store
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000;			--Max navy experience a country can store
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000;				--Max air experience a country can store
+NDefines.NMilitary.PLAN_COHESION_WEIGHTS = { 1.0, 2.0, 80.0 }; 	-- for each cohesion setting, how keen on relocating from distance should we be? (default 1.0), higher weight = shorter max distance
